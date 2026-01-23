ea171bbc6496c517f42ff5cbbefe15b791791b0d
ezequias2d <ezequiasmoises@gmail.com>
2026-01-22 22:14:46 -0300

    Add 'function pointer' delegate constructor, and move constructor and boolean cast operator

--- libs/zabato/include/zabato/delegate.hpp
+++ libs/zabato/include/zabato/delegate.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <type_traits>
+
 namespace zabato
 {
 
@@ -11,6 +13,31 @@ public:
     typedef Ret (*stub_type)(void *object, Args...);
 
     delegate() : m_object(nullptr), m_stub(nullptr) {}
+
+    // Constructor for function pointers (explicit)
+    delegate(Ret (*ptr)(Args...)) { *this = from_ptr(ptr); }
+
+    // Template constructor for arbitrary callables (functors/lambdas)
+    template <
+        typename F,
+        typename = typename std::enable_if<
+            !std::is_same<typename std::decay<F>::type, delegate>::value>::type>
+    delegate(F &&f)
+    {
+        using FunctorType = typename std::decay<F>::type;
+        if constexpr (std::is_convertible_v<FunctorType, Ret (*)(Args...)>)
+        {
+            // Stateless lambda or function pointer -> optimized path
+            Ret (*ptr)(Args...) = static_cast<Ret (*)(Args...)>(f);
+            *this               = from_ptr(ptr);
+        }
+        else
+        {
+            // Stateful lambda / functor -> store reference (CAUTION: lifetime)
+            m_object = (void *)&f;
+            m_stub   = &functor_stub<typename std::remove_reference<F>::type>;
+        }
+    }
 
     template <class T, Ret (T::*TMethod)(Args...)>
     static delegate from_method(T *object)
@@ -41,6 +68,7 @@ public:
     Ret call(Args... args) const { return m_stub(m_object, args...); }
 
     bool is_valid() const { return m_stub != nullptr; }
+    operator bool() const { return is_valid(); }
 
     bool operator==(const delegate &other) const
     {
@@ -73,6 +101,11 @@ private:
     {
         auto p = reinterpret_cast<Ret (*)(Args...)>(object);
         return p(args...);
+    }
+
+    template <typename F> static Ret functor_stub(void *object, Args... args)
+    {
+        return (*static_cast<F *>(object))(args...);
     }
 };
 
